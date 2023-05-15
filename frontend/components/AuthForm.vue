53b8dcb62a2c94e8785b88194a30393473bbaf75
ak-hayashi <eight.kayst@gmail.com>
2023-05-16 00:53:13 +0900

    Create: AuthForm component

---
+++ frontend/components/AuthForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const emailAddress = ref("");
+const password = ref("");
+
+const login = () => {
+  console.log(emailAddress, password)
+}
+</script>
+
+<template>
+  <div>
+    <v-text-field v-model="emailAddress" label="メールアドレス" />
+    <v-text-field v-model="password" label="パスワード" />
+    <v-btn @click="login()">ログイン</v-btn>
+  </div>
+</template>
