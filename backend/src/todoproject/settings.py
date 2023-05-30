1ec5bf1dea1a737ddf79b67fffd0892b29f8ac9b
ak-hayashi <eight.kayst@gmail.com>
2023-05-30 23:07:31 +0900

    Add: install django-cors-headers

--- backend/src/todoproject/settings.py
+++ backend/src/todoproject/settings.py
@@ -35,12 +35,17 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    # 3rd party
+    "rest_framework",
+    "corsheaders",
+    # Local
     "todos",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -131,3 +136,8 @@ except ImportError:
     pass
 
 SECRET_KEY = SECRET_KEY
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
